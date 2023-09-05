51abafcacdd552777eced68a37b16e941917cd40
narkunan <narkunan9585@gmail.com>
2023-09-05 13:15:28 +0530

    database file added

---
+++ database/Dump.sql
@@ -0,0 +1,164 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bookstore
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `bookid` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(90) DEFAULT NULL,
+  `sub_category` varchar(90) DEFAULT NULL,
+  `title` varchar(70) DEFAULT NULL,
+  `coverpage` varchar(30) DEFAULT NULL,
+  `authorname` varchar(30) NOT NULL,
+  `description` text,
+  `authorid` int NOT NULL,
+  `price` float NOT NULL,
+  `stock` int NOT NULL,
+  `sales_count` int DEFAULT NULL,
+  `published_Date` date DEFAULT NULL,
+  PRIMARY KEY (`bookid`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (13,'computer science engineering','DataBase','SQL All - In - One','sql.jpg','nark','Sql is the international standard language for creating and maintaining relational database',1,5088,9,22,NULL),(14,'computer science engineering','collabrate','GitHub for Dummies','git.webp','nark','Software is rarely written by a lone wolf developer, cranking out thousands of lines of code',1,509,21,10,NULL),(15,'computer science engineering','os','Operating Systems','operating.webp','nark','Pearson presents the ninth edition of Operating Systems: Internals and Design Principles',1,200,5,18,NULL),(17,'Electrical Engineering','Microcontroller','Microcontrollers','micr.webp','nark','This textbook covers the hardware and software features of the 8051 in a systematic manner.',1,200,13,10,NULL),(18,'Mechanical Engineering','Fluid Mechanics','Fluid Mechanics','fluid.webp','nark','Fundamentals of Fluid Mechanics offer comprehensive topical coverage, with varied',1,501,14,10,NULL),(24,'Electrical Engineering','programming','pyhton program','pollution.jpg','nark','he display() method is a shortcut to output the rendered template.  You can also load and render the template in one fell swoop:  echo $twig->render(\'index.html\', [\'the\' => \'variables\', \'go\' => \'here\']);',1,21,2,0,'2023-08-23'),(26,'Electrical Engineering','Digital Electronics','electricals circuit','electricals.jpg','narkunanAuthor','The revision of this extremely popular text, Circuits and Networks: Analysis and Synthesis, comes at a time when the industry is increasingly looking to hire engineers who are able to display learning outcomes. The book has been revised based on internationally accepted',2,21,2,0,'2023-09-01'),(27,'Civil Engineering','programming','construction','civilBook.jpg','NarkunanDual','Reading books is a kind of enjoyment. Reading books is a good habit. We bring you a different kinds of books. You can carry this book where ever you want. It is easy to carry. It can be an ideal gift to yourself and to your loved ones. Care instruction keep away from fire',1,21,23,0,'2023-09-03');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `bookid` int NOT NULL,
+  `order_date` date DEFAULT NULL,
+  `ordervalue` int DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (13,1,7,'2023-08-22',1506),(14,1,4,'2023-08-23',40000),(15,1,7,'2023-08-27',502),(16,3,7,'2023-08-28',502),(17,3,13,'2023-08-30',508),(18,3,13,'2023-08-30',508),(19,3,13,'2023-08-30',508),(20,3,13,'2023-08-30',508),(21,3,15,'2023-08-30',800),(22,1,13,'2023-09-01',2032),(23,1,15,'2023-09-03',800),(24,1,13,'2023-09-04',20352);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `roleid` int NOT NULL AUTO_INCREMENT,
+  `roles` varchar(30) NOT NULL,
+  PRIMARY KEY (`roleid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'author'),(2,'user');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `roleid` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,1,1),(2,2,1),(3,3,2),(4,1,2);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'narkunand@gm.com','NarkunanD','narkunan'),(2,'narkunanauthor@gm.com','narkunanAuthor','narkunan'),(3,'narkunanuser@gm.com','narkunanUsers','narkunan');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-05 12:49:15
